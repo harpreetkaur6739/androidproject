54ff4fa189eab5493ed3e459da2113611a10e726
Andre Lemos <andrelemos@Andres-MacBook-Pro.local>
2018-11-30 13:09:36 -0500

    Implement the Admin size

--- COMP304003_FinalProject/app/src/main/java/com/example/comp304_003_finalproject/MainActivity.java
+++ COMP304003_FinalProject/app/src/main/java/com/example/comp304_003_finalproject/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.comp304_003_finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bntAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bntAdmin = (Button) findViewById(R.id.btnAdminLogin);
+
+        bntAdmin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+                Intent intent=new Intent(MainActivity.this, AdminHome.class);
+
+                startActivity(intent);
+
+            }
+        });
+
     }
 
     protected void loginAsUser(View v){
