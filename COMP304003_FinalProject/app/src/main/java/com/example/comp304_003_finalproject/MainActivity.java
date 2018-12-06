14e702f5c68ee2c5aa5abd50442342e464a23333
Harpreetkaur6739 <harpreet.kaur6739@gmail.com>
2018-12-05 23:14:04 -0500

    Online Image Resource

--- COMP304003_FinalProject/app/src/main/java/com/example/comp304_003_finalproject/MainActivity.java
+++ COMP304003_FinalProject/app/src/main/java/com/example/comp304_003_finalproject/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.comp304_003_finalproject;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.comp304_003_finalproject.database.DatabaseHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     Button bntAdmin;
     DatabaseHandler dbHandler;
+    final private int REQUEST_INTERNET = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,11 +49,79 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Download Image from Internet
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.INTERNET},
+                    REQUEST_INTERNET);
+
+        } else{
+            //call this asynchronously
+            new DownloadImageTask().execute("http://ruspravda.info/images/original/Tracking-recruitment-image.jpg");
+        }
+
     }
 
     protected void loginAsUser(View v){
         Intent intent = new Intent(MainActivity.this, UserLogin.class);
         intent.putExtra("user","user");
         startActivity(intent);
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        protected Bitmap doInBackground(String... urls) {
+            return DownloadImage(urls[0]);
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            ImageView img = (ImageView) findViewById(R.id.imgLogo);
+            img.setImageBitmap(result);
+        }
+    }
+
+    private Bitmap DownloadImage(String URL)
+    {
+        Bitmap bitmap = null;
+        InputStream in = null;
+        try {
+            in = OpenHttpConnection(URL);
+            bitmap = BitmapFactory.decodeStream(in);
+            in.close();
+        } catch (IOException e1) {
+            Log.d("NetworkingActivity", e1.getLocalizedMessage());
+        }
+        return bitmap;
+    }
+
+    private InputStream OpenHttpConnection(String urlString) throws IOException
+    {
+        InputStream in = null;
+        int response = -1;
+
+        URL url = new URL(urlString);
+        URLConnection conn = url.openConnection();
+
+        if (!(conn instanceof HttpURLConnection))
+            throw new IOException("Not an HTTP connection");
+        try{
+            HttpURLConnection httpConn = (HttpURLConnection) conn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            response = httpConn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        }
+        catch (Exception ex)
+        {
+            Log.d("Networking", ex.getLocalizedMessage());
+            throw new IOException("Error connecting");
+        }
+        return in;
     }
 }
